ae9ae6f52706158d8443516ea21c8370f1de5374
OLaprevote <olaprevote@protonmail.com>
2020-10-28 16:48:48 +0100

    Put Pose monkey-patching utilities in pythonizer

--- pyrolyse/pose.py
+++ pyrolyse/pose.py
@@ -1,11 +1,6 @@
 """
 TODO
 
-Read/Set property
------------------
-fold_tree   # Read & set property
-pdb_info    # Read & set property
-
 Other ideas
 -----------
 conformation # Read-only attribute
@@ -31,49 +26,19 @@ secstruct   # pose.secstruct[24] returns secstruct of residue 24?
 set_secstruct   # then settable for range of residues.
 """
 
-from copy import copy
 from pyrosetta.rosetta.core.pose import (make_pose_from_sequence,
                                          PDBInfo, Pose)
+
+from .pythonizer.pose import TorsionList, torsion_list_property
 
 
 __all__ = ['Pose', 'pose_from_sequence']
 
-class TorsionList(list):
-    def __init__(self, pose_instance, torsion_setter, *args):
-        self.pose_instance = pose_instance
-        self.torsion_setter = torsion_setter
-        super().__init__(*args)
-
-    def __setitem__(self, index, value):
-        if isinstance(index, int):
-            self.torsion_setter(self.pose_instance, index+1, value)
-
-        # Slices are obtained when using semi-colons to get items, e.g. a[2:8:3]
-        elif isinstance(index, slice):
-            # Find concerned residue indexes
-            residues = range(1, self.pose_instance.size+1)[index]
-            for resid, new_psi in zip(residues, value):
-                self.torsion_setter(self.pose_instance, resid, new_psi)
-
-        else: raise TypeError(("TorsionList indices must be integers or"
-                               " slices, not {}").format(type(index).__name__))
-
-
-def torsion_list_property(getter, setter):
-    def get_torsions(pose_instance):
-        torsion_list = (getter(pose_instance, resid) for resid in range(1, pose_instance.size+1))
-        return TorsionList(pose_instance, setter, torsion_list)
-
-    def set_torsions(pose_instance, new_torsions):
-        for resid in range(pose_instance.size):
-            setter(pose_instance, resid+1, new_torsions[resid])
-
-    return property(get_torsions, set_torsions)
-
 
 def pose_from_sequence(seq, res_type="fa_standard", auto_termini=True):
-    """Returns a pose from single-letter protein sequence. !!! pyrolyse version
+    """Returns a pose from single-letter protein sequence.
 
+    Modified from pyrosetta.
     Returns a Pose object generated from a single-letter sequence of amino acid
     residues in <seq> using the <res_type> ResidueType and creates N- and C-
     termini if <auto_termini> is set to True.
@@ -136,3 +101,8 @@ for attr in _torsions_attributes:
     getter = getattr(Pose, attr)
     setter = getattr(Pose, 'set_{}'.format(attr))
     setattr(Pose, torsion_list_name, torsion_list_property(getter, setter))
+
+
+# Read/Set property
+Pose.fold_tree = property(Pose.fold_tree, Pose.fold_tree)
+Pose.pdb_info = property(Pose.pdb_info, Pose.pdb_info)
---
+++ pyrolyse/pythonizer/pose.py
@@ -0,0 +1,31 @@
+class TorsionList(list):
+    def __init__(self, pose_instance, torsion_setter, *args):
+        self.pose_instance = pose_instance
+        self.torsion_setter = torsion_setter
+        super().__init__(*args)
+
+    def __setitem__(self, index, value):
+        if isinstance(index, int):
+            self.torsion_setter(self.pose_instance, index+1, value)
+
+        # Slices are obtained when using semi-colons to get items, e.g. a[2:8:3]
+        elif isinstance(index, slice):
+            # Find concerned residue indexes
+            residues = range(1, self.pose_instance.size+1)[index]
+            for resid, new_psi in zip(residues, value):
+                self.torsion_setter(self.pose_instance, resid, new_psi)
+
+        else: raise TypeError(("TorsionList indices must be integers or"
+                               " slices, not {}").format(type(index).__name__))
+
+
+def torsion_list_property(getter, setter):
+    def get_torsions(pose_instance):
+        torsion_list = (getter(pose_instance, resid) for resid in range(1, pose_instance.size+1))
+        return TorsionList(pose_instance, setter, torsion_list)
+
+    def set_torsions(pose_instance, new_torsions):
+        for resid in range(pose_instance.size):
+            setter(pose_instance, resid+1, new_torsions[resid])
+
+    return property(get_torsions, set_torsions)
