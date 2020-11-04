6d292f707178bd7b2f73324da4ccf3bbe780b1ba
OLaprevote <olaprevote@protonmail.com>
2020-11-04 17:12:06 +0100

    Comment and pretty code SmallMover

--- pyrolyse/movers/simple.py
+++ pyrolyse/movers/simple.py
@@ -10,12 +10,14 @@ __all__ = ['SmallMover']
 # TODO find a way to change "path" of class.
 # Child class not possible because of virtual functions.
 
+# movemap can now also be set using mover.movemap = new_movemap
+# One pitt fall, though: writing mover.movemap = pose would
+# be the same as writing mover.movemap(pose).
 SmallMover.movemap = CallableProperty(SmallMover.movemap.__call__,
                                       SmallMover.movemap.__get__,
                                       SmallMover.movemap)
 
-SmallMover.temperature = property(SmallMover.temperature,
-                                  SmallMover.temperature)
+SmallMover.temperature = property(SmallMover.temperature, SmallMover.temperature)
 SmallMover.nmoves = property(SmallMover.nmoves, SmallMover.nmoves)
 SmallMover.angles_max = property(get_angles_max, set_angles_max)
 
@@ -25,6 +27,7 @@ SmallMover.__call__ = SmallMover.apply
 # TODO wrong doc.
 SmallMover.__doc__ = """Mover perturbing phi and psi of a random residue
 
+Modified from PyRosetta by pyrolyse.
 Such that they create a 'shearing' effect, minimizing the downstream
 consequences of this torsional perturbation. The final torsion angle
 is subject to a metropolis criterion using the rama score to ensure that
@@ -40,8 +43,8 @@ temperature_in: float
 nmoves_in: int
 
 2.
-arg0: pyrosetta.rosetta.protocols.simple_moves.ShearMover
-    If the only input in a ShearMover object, copies it.
+arg0: pyrosetta.rosetta.protocols.simple_moves.SmallMover
+    If the only input in a SmallMover object, copies it.
 
 Attributes
 ----------
