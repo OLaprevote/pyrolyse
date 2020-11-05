4bc14407d44ea31fdeb818c081f119d7ca92bc25
OLaprevote <olaprevote@protonmail.com>
2020-11-05 11:58:43 +0100

    Correct get_pose tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,16 +1,18 @@
 import pytest
+import sys
+from pathlib import Path
 
 from pyrolyse.utils import init, get_pose
 
 init()
 
-
+pdb_path = Path(sys.path[0], 'data', '1ubq.pdb')
 @pytest.mark.parametrize('pose_input,kind',
-                         [('data/1ubq.pdb', 'file'), ('PYTEST', 'sequence'),
+                         [(pdb_path, 'file'), ('PYTEST', 'sequence'),
                           ('1UBQ', 'rcsb')])
 def test_get_pose(pose_input, kind):
     assert get_pose(pose_input, kind)
 
-@pytest.mark.parametrize('pose_input', ['data/1ubq.pdb', 'PYTEST', '1UBQ'])
+@pytest.mark.parametrize('pose_input', [pdb_path, 'PYTEST', '1UBQ'])
 def test_auto_get_pose(pose_input):
     assert get_pose(pose_input)
