361e3ed4368bebd082ba244f6164acfd22b37bb1
OLaprevote <olaprevote@protonmail.com>
2020-11-07 17:09:03 +0100

    Remove Warnings upon Pose assertion

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,8 +11,8 @@ pdb_path = Path(sys.path[0], 'files', '1ubq.pdb')
                          [(pdb_path, 'file'), ('PYTEST', 'sequence'),
                           ('1UBQ', 'rcsb')])
 def test_get_pose(pose_input, kind):
-    assert get_pose(pose_input, kind)
+    assert get_pose(pose_input, kind).residue(1)
 
 @pytest.mark.parametrize('pose_input', [pdb_path, 'PYTEST', '1UBQ'])
 def test_auto_get_pose(pose_input):
-    assert get_pose(pose_input)
+    assert get_pose(pose_input).residue(1)
