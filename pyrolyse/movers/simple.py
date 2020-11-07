de05596c5d6bfa5b3bc7f521e7372ad3824a1196
OLaprevote <olaprevote@protonmail.com>
2020-11-07 01:12:03 +0100

    Monkey-patch roughly ShearMover

--- pyrolyse/movers/simple.py
+++ pyrolyse/movers/simple.py
@@ -1,4 +1,4 @@
-from pyrosetta.rosetta.protocols.simple_moves import SmallMover
+from pyrosetta.rosetta.protocols.simple_moves import SmallMover, ShearMover
 
 from ..pythonizer.mover.simple import (CallableProperty, get_angles_max,
                                        set_angles_max)
@@ -6,9 +6,6 @@ from ..pythonizer.mover.simple import (CallableProperty, get_angles_max,
 
 __all__ = ['SmallMover']
 
-
-# TODO find a way to change "path" of class.
-# Child class not possible because of virtual functions.
 
 # movemap can now also be set using mover.movemap = new_movemap
 # One pitt fall, though: writing mover.movemap = pose would
@@ -21,15 +18,12 @@ SmallMover.temperature = property(SmallMover.temperature, SmallMover.temperature
 SmallMover.nmoves = property(SmallMover.nmoves, SmallMover.nmoves)
 SmallMover.angles_max = property(get_angles_max, set_angles_max)
 
-# Make SmallMover callable
 SmallMover.__call__ = SmallMover.apply
+SmallMover.__doc__ = """Mover randomly perturbing phi and psi of a residue
 
-# TODO wrong doc.
-SmallMover.__doc__ = """Mover perturbing phi and psi of a random residue
-
-Modified from PyRosetta by pyrolyse.
-Such that they create a 'shearing' effect, minimizing the downstream
-consequences of this torsional perturbation. The final torsion angle
+A mover that makes independent random perturbations of the phi and
+psi torsion angles of residue i. It selects residue i at random among
+movable residues (set by its MoveMap), and the final torsion angle
 is subject to a metropolis criterion using the rama score to ensure that
 only favorable backbone torsion angles are being selected. The number of
 perturbations, and the magnitude of perturbations, and the temperature
@@ -37,12 +31,13 @@ in the rama check, can all be modified.
 
 Parameters
 ----------
-1.
+1. No argument
+2.
 movemap_in: pyrosetta.rosetta.core.kinematics.MoveMap
 temperature_in: float
 nmoves_in: int
 
-2.
+3.
 arg0: pyrosetta.rosetta.protocols.simple_moves.SmallMover
     If the only input in a SmallMover object, copies it.
 
@@ -56,4 +51,69 @@ Methods
 -------
 apply
 angle_max
+
+Examples
+--------
+>>> mmap = lys.MoveMap(bb=True)
+>>> small_mv = lys.movers.simple.SmallMover(mmap, 1., 1)
+>>> small_mv.angles_max = {'H':5, 'E': 10, 'L':15}
+>>> pose = lys.get_pose('A'*5)
+>>> small_mv(pose)
+"""
+
+
+# Make movemap settable
+ShearMover.movemap = CallableProperty(ShearMover.movemap.__call__,
+                                      ShearMover.movemap.__get__,
+                                      ShearMover.movemap)
+
+ShearMover.temperature = property(ShearMover.temperature, ShearMover.temperature)
+ShearMover.nmoves = property(ShearMover.nmoves, ShearMover.nmoves)
+ShearMover.angles_max = property(get_angles_max, set_angles_max)
+
+# Make ShearMover callable
+ShearMover.__call__ = ShearMover.apply
+
+ShearMover.__doc__ = """Mover perturbing torsion angles to create a shear effect
+
+Modified from PyRosetta by pyrolyse.
+A mover that perturbs the phi of residue i and the psi of residue
+i-1 such that they create a 'shearing' effect, minimizing the downstream
+consequences of this torsional perturbation. The final torsion angle
+is subject to a metropolis criterion using the rama score to ensure that
+only favorable backbone torsion angles are being selected. The number of
+perturbations, and the magnitude of perturbations, and the temperature
+in the rama check, can all be modified.
+
+Parameters
+----------
+1. No argument.
+2.
+movemap_in: pyrosetta.rosetta.core.kinematics.MoveMap
+temperature_in: float
+nmoves_in: int
+
+3.
+arg0: pyrosetta.rosetta.protocols.simple_moves.ShearMover
+    If the only input in a ShearMover object, copies it.
+
+Attributes
+----------
+temperature
+nmoves
+angles_max: lys.pythonize.movers.simple.AngleMaxDict
+    Inherited from dict. Get and set angle_max instead of items.
+
+Methods
+-------
+apply
+angle_max
+
+Examples
+--------
+>>> mmap = lys.MoveMap(bb=True)
+>>> shear_mv = lys.movers.simple.ShearMover(mmap, 1., 1)
+>>> shear_mv.angles_max = {'H':5, 'E': 10, 'L':15}
+>>> pose = lys.get_pose('A'*5)
+>>> shear_mv(pose)
 """
