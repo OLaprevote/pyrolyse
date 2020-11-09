fa210f9cd53e7e27e7b9de596faac36407b98df7
OLaprevote <olaprevote@protonmail.com>
2020-11-10 00:41:00 +0100

    Import recently added objects

--- pyrolyse/all.py
+++ pyrolyse/all.py
@@ -1,8 +1,9 @@
-from . import core, movers, pythonizer
+from . import core, movers, bindings
 
 from .movers.moves import SequenceMover, RepeatMover
 from .core.pose import Pose
 from .core.residue import Residue
 from .core.pdbinfo import PDBInfo
+from .core.movemap import MoveMap
 
 from .utils import *
--- pyrolyse/movers/__init__.py
+++ pyrolyse/movers/__init__.py
@@ -1,2 +1,2 @@
-from . import simple
+from . import simple, min_pack
 from .moves import SequenceMover, RepeatMover
